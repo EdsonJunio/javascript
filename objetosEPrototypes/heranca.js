fd76bd52ad6c12b4247bacc5891bed024dfede7e
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2023-11-14 23:49:32 -0300

    js

--- objetosEPrototypes/heranca.js
+++ objetosEPrototypes/heranca.js
@@ -8,4 +8,20 @@ function Produto(nome, preco) {
 
 Produto.prototype.aumento =  function(quantia) {
     this.preco += quantia;
+};
+
+Camiseta.prototype.desconto =  function(quantia) {
+    this.preco -= quantia;
+};
+
+function  Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
 }
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+
+const camiseta = new Camiseta('regata', 2.9, 'preta');
+const produto = new Camiseta('gen', 7.9);
+//camiseta.aumento(10);
+console.log(camiseta);
